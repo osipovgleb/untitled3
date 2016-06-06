1349053f0ea4c3eb06de66cfd058e6a7f0cfcdde
osipovgleb <osglebs@gmail.com>
2016-06-06 15:08:33 +0400

    3 upload
    Its working

--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,11 @@
-/*05.06.2016 Osipov Gleb 
- *the start block of the program*/
+<?php /* 05.06.2016 Osipov Gleb the start block of the program */
+$title = $args[0];
+?>
+
 <html>
 <head>
-    <title> <?php
-     $title
-     ?> 
-</title>
+    <title><?php echo $title; ?></title>
 </head>
+
 <body>
 <p>hi world</p>
