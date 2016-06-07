1afa816fa2c55f58cd57ff2e025b43fcbfa5cfff
osipovgleb <osglebs@gmail.com>
2016-06-07 14:24:29 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 /*05.06.2016 Osipov Gleb */
 /*includes classes to type on screen*/
 include("classes/html.php");
+include("classes/session.php");
+
+Session::start_session();
+Session::store_session();
+
 HTML::header("test1");
 HTML::template("index");
 HTML::footer();
 HTML::flush();
+
+//Session::finish_session();
 ?>
 
 
